567670e9e4c856164fc249d131e4629f389b5b73
erhode712 <erhode3@unl.edu>
2024-09-20 11:14:34 -0500

    added my major to the output

---
+++ hello/hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Emma Rhode
+ *         erhode3@huskers.unl.edu
+ * Date: 2024/08/30
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World, I'm Emma Rhode\n");
+  printf("My major is Computer Science.\n");
+
+
+  return 0;
+}
